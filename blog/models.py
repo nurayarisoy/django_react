1ce2a4c9226a5202651b8a6cb92577a44834a1b5
SUAT ARISOY <cevriyeceven@gmail.com>
2022-03-21 20:21:53 +0100

    feat:add blog

--- blog/models.py
+++ blog/models.py
@@ -1,54 +1,59 @@
+from unicodedata import category
+
 from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
-
-
 class Post(models.Model):
-    user = models.ForeignKey(User,
-                             on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
-    content = models.TextField()
-    image = models.ImageField(
-        upload_to='post_pics', blank=True)
-    publish_date = models.DateTimeField(auto_now_add=True)
-    last_updated = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    content = models.TextField(max_length=1000)
+    image = models.CharField(max_length=1000)
+    createdDate = models.DateTimeField(auto_now_add=True)
+    updatedDate= models.DateTimeField(auto_now=True)
     POST_CATEGORY_CHOICES = [
-        ('PY', 'Python'),
-        ('JS', 'Javascript'),
-        ('DJ', 'Django'),
-        ('CSS', 'CSS'),
-        ('HTML', 'HTML'),
+        ('General', 'General'),
+        ('Technology', 'Technology'),
+        ('Business', 'Business'),
+        ('Entertainment', 'Entertainment'),
+        ('Health', 'Health'),
+        ('Science', 'Science'),
+        ('Sports', 'Sports')
     ]
     category = models.CharField(
-        max_length=4, choices=POST_CATEGORY_CHOICES, default='DJ')
-    POST_STATUS_CHOICES = [
-        ('D', 'Draft'),
-        ('P', 'Published'),
-    ]
-    status = models.CharField(
-        max_length=1, choices=POST_STATUS_CHOICES, default='D')
+        max_length=20, choices=POST_CATEGORY_CHOICES, default='General')
 
     def __str__(self):
-        return f"{self.title}"
+        return f"{self.user} {self.title} {self.content}"
 
 
 class Comment(models.Model):
-    user = models.ForeignKey(User,
-                             on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    content = models.TextField()
-    publish_date = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length=1000)
+    createdDate = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} {self.content}"
 
 
 class Like(models.Model):
-    user = models.ForeignKey(User,
-                             on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='likes')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    createdDate = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} {self.post}"
 
 
 class PostView(models.Model):
-    user = models.ForeignKey(User,
-                             on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    time_stamp = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='views')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    createdDate = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user} {self.post}"
--- main/urls.py
+++ main/urls.py
@@ -13,17 +13,40 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
+
 from django.views.static import serve
+
+
+from django.contrib import admin
+from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
 from rest_framework import permissions
+from django.conf import settings
 
-
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Django React Blog API",
+        default_version='v1',
+        description="Django React Blog API",
+        terms_of_service="#",
+        contact=openapi.Contact(email="hsyn06arslan@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("users/", include("users.urls")),
+    path('swagger(<format>\.json|\.yaml)', schema_view.without_ui(
+        cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger',
+         cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc',
+         cache_timeout=0), name='schema-redoc'),
+    path('__debug__/', include('debug_toolbar.urls')),
+    path('users/', include('users.urls')),
     path('blog/', include('blog.urls')),
 ]
 if settings.DEBUG:
